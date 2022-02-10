9bd2eb5dd2831f10ded650c018fe2ed432459697
Chauhan <nakulchauhan@cppib.com>
2022-02-10 09:20:40 +0530

    Same tree

--- src/com/BreadthFirstSearch/SameTree.java
+++ src/com/BreadthFirstSearch/SameTree.java
@@ -1,5 +1,18 @@
 package com.BreadthFirstSearch;
 
 public class SameTree {
-    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        }
+
+        if ((p.val != q.val)) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
