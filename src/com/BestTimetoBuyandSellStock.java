1d23a75f8585f7367f683317865d81faf31477a3
Nakul Chauhan <nkul.ic20@gmail.com>
2023-03-06 20:58:47 +0530

    Update BestTimetoBuyandSellStock.java

--- src/com/BestTimetoBuyandSellStock.java
+++ src/com/BestTimetoBuyandSellStock.java
@@ -2,9 +2,11 @@ package com;
 
 public class BestTimetoBuyandSellStock {
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 1, 25, 4}));
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(allProfits(new int[]{7, 1, 5, 3, 6, 4}));//1-5 = 4, 3-6 = 3, Total = 7
     }
 
+    //just one maximum profit
     public static int maxProfit(int[] prices) {
         int left = 0;    //buy
         int right = 1;   //sell
@@ -23,5 +25,24 @@ public class BestTimetoBuyandSellStock {
         }
 
         return profit;
+    }
+
+    public static int allProfits(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int allProfits = 0;
+
+        while (left < right && right < prices.length) {
+            if (prices[right] > prices[left]) {
+                allProfits += (prices[right] - prices[left]);
+                left = right;
+                right++;
+            } else {
+                left++;
+                right++;
+            }
+        }
+
+        return allProfits;
     }
 }
