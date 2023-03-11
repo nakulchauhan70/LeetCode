525e891c6bf56269ee3989b2dbaeb4f656db3a9d
Nakul Chauhan <nakulchauhan@cppib.com>
2023-03-11 18:01:12 +0530

    Create PalindromeNumber.java

---
+++ src/com/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package com;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+
+        int n = str.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
