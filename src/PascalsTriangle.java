877f14cc54736ad76d61c782bfb220dfd00f9cd9
Nakul Chauhan <nakulchauhan@cppib.com>
2023-03-12 11:01:34 +0530

    Update PascalsTriangle.java

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -9,6 +9,14 @@ public class PascalsTriangle {
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalsTriangle = new ArrayList<>();
 
+        //1,4,6,4,1
+        //i=1 => 1 =>     1
+        //i=2 => 2 =>    1,1
+        //i=3 => 3 =>   1,2,1
+        //1=4 => 4 =>  1,3,3,1
+        //i=5 => 5 => 1,4,6,4,1
+        //* pattern
+
         if (1 <= numRows) {
             pascalsTriangle.add(new ArrayList<Integer>() {{
                 add(1);
