d95b20aeb0540e0219bbcaea12225e5db23d99de
Nakul Chauhan <nakulchauhan@cppib.com>
2024-11-24 18:35:30 +0530

    Update NoofSegmentsInaString.java

--- src/com/NoofSegmentsInaString.java
+++ src/com/NoofSegmentsInaString.java
@@ -5,12 +5,12 @@ import java.util.stream.Collectors;
 
 public class NoofSegmentsInaString {
 
-    public int countSegments(String s) {
+    public static int countSegments(String s) {
         return (int) Arrays.stream(s.split(" ")).filter(s1 -> !s1.isEmpty()).map(String::trim).count();
     }
 
     public static void main(String[] args) {
-        System.out.println("     ".trim().length());
+        System.out.println(countSegments("     "));
     }
 }
 
