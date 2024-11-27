bf87b883b53ce333800e32e58658d5ecc0bbc1a7
Nakul Chauhan <nakulchauhan@cppib.com>
2024-11-27 21:05:15 +0530

    Update RemoveDuplicatesfromSortedArray.java

--- src/com/array/RemoveDuplicatesfromSortedArray.java
+++ src/com/array/RemoveDuplicatesfromSortedArray.java
@@ -13,7 +13,7 @@ public class RemoveDuplicatesfromSortedArray {
         int count = 1;
         int temp = nums[0];
         int nextIndexToFill = 1;
-
+//0,0,1,1,2,2
         for (int i = 1; i < nums.length; i++) {
             if (temp != nums[i]) {
                 temp = nums[i];
@@ -22,16 +22,26 @@ public class RemoveDuplicatesfromSortedArray {
                 nextIndexToFill++;
             }
         }
+        return count;
+    }
 
-//        for (int num : nums) {
-//            if (temp != num) {
-//                temp = num;
-//                count++;
-//            }
-//        }
+    public static int removeDuplicates2(int[] nums) {
+        if (nums.length == 0) {
+            return 1;
+        }
 
-//        System.out.println(Arrays.toString(nums));
-
+        int count = 1;
+        int temp = nums[0];
+        int nextIndexToFill = 1;
+//0,0,1,1,2,2
+        for (int i = 1; i < nums.length; i++) {
+            if (temp != nums[i]) {
+                temp = nums[i];
+                nums[nextIndexToFill] = nums[i];
+                count++;
+                nextIndexToFill++;
+            }
+        }
         return count;
     }
 }
