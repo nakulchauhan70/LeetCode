037aac05327d1061fb59070348072ada796475a4
Nakul Chauhan <nakulchauhan@cppib.com>
2024-12-01 19:51:14 +0530

    Update LongestCommonPrefix.java

--- src/com/LongestCommonPrefix.java
+++ src/com/LongestCommonPrefix.java
@@ -2,13 +2,29 @@ package com;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
+        System.out.println("flower".indexOf("flow"));
+        System.out.println("flow".indexOf("flower"));
 
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        for (String str : strs) {
-
+        if (strs == null || strs.length == 0) {
+            return "";
         }
-        return null;
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
     }
 }
