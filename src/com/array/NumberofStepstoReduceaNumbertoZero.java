dcfdfb6e37656e7b31fa55edbb517c59713bdf6a
Nakul Chauhan <nakulchauhan@cppib.com>
2024-12-20 20:41:35 +0530

    Create NumberofStepstoReduceaNumbertoZero.java

---
+++ src/com/array/NumberofStepstoReduceaNumbertoZero.java
@@ -0,0 +1,17 @@
+package com.array;
+
+public class NumberofStepstoReduceaNumbertoZero {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num != 0) {
+            if (num % 2 == 1) {
+                num--;
+            } else {
+                num = num / 2;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
